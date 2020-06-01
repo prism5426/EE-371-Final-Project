dd0b94d3f0dd154665ef7627d706f0f8fc6b3d56
Issac PC <jy98@uw.edu>
2020-06-01 13:24:45 -0700

    initial commit for pipe and game logic

---
+++ pipe.sv
@@ -0,0 +1,8 @@
+module pipe(clk, resetGame, press, x, y, r, g, b);
+	input logic clk, resetGame, press;
+    input logic [9:0] x;
+    input logic [8:0] y;
+    output logic [7:0] r, g, b;
+
+
+endmodule
